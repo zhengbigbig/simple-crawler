d8f03c44c75a28aee3ddd125bf415f97caf65ced
zhengbigbig <780357902@qq.com>
2019-11-02 12:19:57 +0800

    设计并实现基本算法

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+interface InsterestingLink {
+    boolean handleLink();
+}
+
 public class Main {
     public static void main(String[] args) throws IOException {
 
@@ -25,10 +29,7 @@ public class Main {
         Set<String> processedLinks = new HashSet<>();
         linkPool.add("https://sina.cn");
 
-        while (true) {
-            if (linkPool.isEmpty()) {
-                break;
-            }
+        while (!linkPool.isEmpty()) {
             // ArrayList 从尾部删除更有效率,remove 方法会返回要删除的元素
             String link = linkPool.remove(linkPool.size() - 1);
 
@@ -36,48 +37,48 @@ public class Main {
                 continue;
             }
 
-            if (link.contains("//news.sina.cn") || "https://sina.cn".equals(link)) {
-                // 这是我们感兴趣的，我们只处理新浪站
-                CloseableHttpClient httpclient = HttpClients.createDefault();
-//                System.out.println(link);
-                if(link.startsWith("//")){
-                    link = "https:" + link;
-                }
-
-                HttpGet httpGet = new HttpGet(link);
-                httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36");
-                CloseableHttpResponse response1 = httpclient.execute(httpGet);
-                try {
-//                    System.out.println(response1.getStatusLine());
-
-                    if(response1.getStatusLine().getStatusCode() == 200){
-                        HttpEntity entity1 = response1.getEntity();
-                        String html = EntityUtils.toString(entity1);
-
-                        Document doc = Jsoup.parse(html);
-
-                        Elements elements = doc.select("a");
-                        for (Element element : elements) {
-                            linkPool.add(element.attr("href"));
-                        }
-
-                        // 假如这是一个新闻的详细页面，就存入数据库，否则，什么都不做
-                        Elements articles = doc.select("article");
-                        if (!articles.isEmpty()) {
-                            for (Element article : articles) {
-                                String title = article.children().select("h1").text();
-                                String time = article.children().select("time").text();
-                                System.out.println("title = " + title + "time = " + time);
-                            }
-                        }
-                    }
-                    processedLinks.add(link);
-
-                } finally {
-                    response1.close();
-                }
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+                assert doc != null;
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+                // 假如这是一个新闻的详细页面，就存入数据库，否则，什么都不做
+                storeIntoDatabaseIfNewsPage(doc);
+                processedLinks.add(link);
             }
         }
 
+    }
+
+    private static void storeIntoDatabaseIfNewsPage(Document doc) {
+        Elements articles = doc.select("article");
+        if (!articles.isEmpty()) {
+            for (Element article : articles) {
+                String title = article.children().select("h1").text();
+                String time = article.children().select("time").text();
+                System.out.println("title = " + title + "time = " + time);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36");
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            if (response1.getStatusLine().getStatusCode() == 200) {
+                HttpEntity entity1 = response1.getEntity();
+                String html = EntityUtils.toString(entity1);
+                return Jsoup.parse(html);
+            }
+        }
+        return null;
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return link.contains("//news.sina.cn") || "https://sina.cn".equals(link);
     }
 }
